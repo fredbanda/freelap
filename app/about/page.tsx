67eb1f2864f3eb6043c2fc9c04d10a8569417807
Fred Banda <fredcbanda@hotmail.com>
2024-02-26 13:17:17 +0200

    space

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -38,10 +38,10 @@ const AboutUs = () => {
                     d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                     clip-rule="evenodd"></path>
                 </svg>
-                It Takes a village
+                It Takes a village 
               </dt>
               <dd className="inline"> 
-                From the beginning of time Africans have always believed that it takes the whole village to raise a child. Ubuntu which practiced in the entire of Africa believes in giving back to the community.
+                 From the beginning of time Africans have always believed that it takes the whole village to raise a child. Ubuntu which practiced in the entire of Africa believes in giving back to the community.
                 .</dd>
             </div>
             <div className="relative pl-9">
